fa3a24621c52baf62b7f8e77c47617b4984bf38a
Nikita Elizarov <xenilcool@gmail.com>
2017-11-30 19:49:06 +0700

    First commit

---
+++ src/main/java/com/xenil/javatusurtask/demo/JavaTusurTaskApplication.java
@@ -0,0 +1,11 @@
+package com.xenil.javatusurtask.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTusurTaskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTusurTaskApplication.class, args);
+	}
+}
